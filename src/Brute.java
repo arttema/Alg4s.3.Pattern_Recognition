9db786e2653cd493c954aa600fcdf5863a43144e
Artem Stolpovski <ArtemStolpovski@coherentsolutions.com>
2014-03-11 17:32:47 +0300

    Brute algorithm initial

--- src/Brute.java
+++ src/Brute.java
@@ -1,3 +1,49 @@
 public class Brute {
-    public static void main(String[] args){}
+
+    public static void main(String[] args){
+        // rescale coordinates and turn on animation mode
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        StdDraw.show(0);
+
+        // read in the input
+        int inputSize = 0;
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Point[] input = new Point[200];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            Point p = new Point(x, y);
+            p.draw();
+            input[i] = p;
+            inputSize++;
+        }
+
+        Point[] points = new Point[inputSize];
+        System.arraycopy(input, 0, points, 0, inputSize);
+       // points[1].drawTo(points[5]);
+        for (int p = 0; p < points.length-1; p++) {
+            for (int q = 0; q < points.length-1; q++) {
+               // points[p].drawTo(points[q]);
+                for (int r = 0; r < points.length-1; r++) {
+                    for (int s = 0; s < points.length-1; s++) {
+                        double slope1 = points[p].slopeTo(points[q]);
+                        double slope2 = points[p].slopeTo(points[r]);
+                        double slope3 = points[p].slopeTo(points[s]);
+                        if(slope1==slope2 && slope1==slope3 && slope2==slope3){
+                            points[p].drawTo(points[q]);
+                            points[p].drawTo(points[r]);
+                            points[p].drawTo(points[s]);
+                            StdDraw.show(0);
+                            System.out.println(points[p]+" -> "+points[q]+" -> "+points[r]+" -> "+points[s]);
+                        }
+                    }
+                }
+            }
+        }
+
+        StdDraw.show(0);
+    }
 }
--- src/Point.java
+++ src/Point.java
@@ -2,26 +2,62 @@ import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
     // compare points by slope to this point
-    public final Comparator<Point> SLOPE_ORDER = null;
-    // construct the point (x, y)
+   public final Comparator<Point> SLOPE_ORDER = new SlopeComparator<Point>();
+
+    public final int x;
+    public final int y;
+
+    // create the point (x, y)
     public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
     }
+
     // draw this point
     public void draw() {
+          /* DO NOT MODIFY */
+        StdDraw.point(x, y);
     }
+
     // draw the line segment from this point to that point
     public void drawTo(Point that) {
+          /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
     }
+
     // string representation
     public String toString() {
-        return null;
+        return "(" + x + ", " + y + ")";
     }
-    // is this point lexicographically smaller than that point?
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
     public int compareTo(Point that) {
-        return 0;
+        int x1 = that.x, y1 = that.y;
+
+        if (y < y1) return -1;
+        else if (y == y1 && x < x1) return -1;
+        else if (y == y1 && x == x1) return 0;
+        else return 1;
     }
+
     // the slope between this point and that point
     public double slopeTo(Point that) {
-        return 0;
+        double y0 = this.y, x0 = this.x;
+        double y1 = that.y, x1 = that.x;
+
+        if (y1 == y0 && x1 == x0) return Double.NEGATIVE_INFINITY;
+        if (y1 == y0 ) return Double.POSITIVE_INFINITY;
+        if (x1 == x0 ) return x1-x1/x1;
+        return (y1 - y0)/(x1 - x0);
+    }
+
+    private class SlopeComparator<Point> implements Comparator<Point> {
+
+        @Override
+        public int compare(Point o1, Point o2) {
+           return 0;
+        }
     }
 }
