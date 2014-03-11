95b6884d5f41fd19725017a4e6332ad94a648034
Artem Stolpovski <ArtemStolpovski@coherentsolutions.com>
2014-03-11 18:14:23 +0300

    Array list in brute

--- src/Brute.java
+++ src/Brute.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Brute {
 
     public static void main(String[] args){
@@ -6,38 +9,50 @@ public class Brute {
         StdDraw.setYscale(0, 32768);
         StdDraw.show(0);
 
-        // read in the input
+        List<Point> points= new ArrayList<Point>();
+        // read in the points
         int inputSize = 0;
         String filename = args[0];
         In in = new In(filename);
         int N = in.readInt();
-        Point[] input = new Point[200];
         for (int i = 0; i < N; i++) {
             int x = in.readInt();
             int y = in.readInt();
             Point p = new Point(x, y);
             p.draw();
-            input[i] = p;
+            points.add(p);
             inputSize++;
         }
 
-        Point[] points = new Point[inputSize];
-        System.arraycopy(input, 0, points, 0, inputSize);
+        //Point[] points = new Point[inputSize];
+        //System.arraycopy(points, 0, points, 0, inputSize);
+      //  Arrays.sort(points);
        // points[1].drawTo(points[5]);
-        for (int p = 0; p < points.length-1; p++) {
-            for (int q = 0; q < points.length-1; q++) {
-               // points[p].drawTo(points[q]);
-                for (int r = 0; r < points.length-1; r++) {
-                    for (int s = 0; s < points.length-1; s++) {
-                        double slope1 = points[p].slopeTo(points[q]);
-                        double slope2 = points[p].slopeTo(points[r]);
-                        double slope3 = points[p].slopeTo(points[s]);
-                        if(slope1==slope2 && slope1==slope3 && slope2==slope3){
-                            points[p].drawTo(points[q]);
-                            points[p].drawTo(points[r]);
-                            points[p].drawTo(points[s]);
+        Point temppoint;
+        for (int p = 0; p < points.size()-1; p++) {
+            for (int q = 1; q < points.size()-1; q++) {
+               // points[p).drawTo(points[q));
+                for (int r = 2; r < points.size()-1; r++) {
+
+                    for (int s = 3; s < points.size()-1; s++) {
+                        if (points.get(s).x == 1234) {
+                            System.out.println("");
+                        }
+                        //temppoint = points.remove(p);
+                        double slope1 = points.get(p).slopeTo(points.get(q));
+                        double slope2 = points.get(p).slopeTo(points.get(r));
+                        double slope3 = points.get(p).slopeTo(points.get(s));
+                        double slope4 = points.get(q).slopeTo(points.get(s));
+                        double slope5 = points.get(r).slopeTo(points.get(s));
+//                        if (points.get(p).compareTo(points.get(q)) < 0 &&
+//                                points.get(q).compareTo(points.get(r)) < 0 &&
+//                                points.get(r).compareTo(points.get(s)) < 0)
+                            if(slope1==slope2 && slope1==slope3 && slope2==slope3 && slope4 == slope3 && slope2 ==slope5 && slope5 == slope1){
+                            points.get(p).drawTo(points.get(q));
+                            points.get(p).drawTo(points.get(r));
+                            points.get(p).drawTo(points.get(s));
                             StdDraw.show(0);
-                            System.out.println(points[p]+" -> "+points[q]+" -> "+points[r]+" -> "+points[s]);
+                            System.out.println(points.get(p)+" -> "+points.get(q)+" -> "+points.get(r)+" -> "+points.get(s));
                         }
                     }
                 }
